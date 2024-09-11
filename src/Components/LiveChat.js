55eae542082b394f0eb6c5cb907f2dd42d099030
Rakeshsabbani1902 <rakeshsabbani1902@gmail.com>
2024-09-11 12:38:40 +0530

    Update LiveChat.js

--- src/Components/LiveChat.js
+++ src/Components/LiveChat.js
@@ -25,7 +25,7 @@ import { Form } from 'react-router-dom';
     },[])
   return (
     <>
-    <div className=" p-2 ml-2 shadow-sm border-black w-full h-[405px]  bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+    <div className=" w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
        {ChatMessages.map((c,index)=>(
                 //Don't use index as keys- Disclaimer
                 <ChatMessage key ={index} name={c.name} message={c.message}/>
@@ -34,7 +34,7 @@ import { Form } from 'react-router-dom';
     </div>
 
 
-    <form className="w-full p-1 ml-2 border border-black" onSubmit={(e)=>{
+    <form className="w-full p-2 ml-2 border border-black" onSubmit={(e)=>{
         e.preventDefault();
         console.log("form Submit");
         dispatch(addMessage({
@@ -44,7 +44,7 @@ import { Form } from 'react-router-dom';
         setLiveMessage("");
     
     }}>
-    <input className="w-96" type='text' value={liveMessage} onChange={(e)=>{
+    <input className=" px-2 w-96" type='text' value={liveMessage} onChange={(e)=>{
       setLiveMessage(e.target.value);
     }}/>
     <button className="px-2 mx-2 bg-green-200"> Send</button>
