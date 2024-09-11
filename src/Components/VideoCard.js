f5fe0586b2cb7fa36b033c6f54eefafa0d46295e
Rakeshsabbani1902 <rakeshsabbani1902@gmail.com>
2024-09-11 12:48:10 +0530

    Update VideoCard.js

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -6,7 +6,7 @@ const VideoCard = ({info}) => {
     const { channelTitle, title,thumbnails}= snippet;
 
   return (
-    <div className='p-2 m-2 w-60  shadow-lg'>
+    <div className='p-2 m-2 w-60 h-80 shadow-lg'>
         <img alt="thumbnail" className='rounded-lg' src={thumbnails.medium.url}/>
         <ul>
             <li className='font-bold py-2'>{title}</li>
